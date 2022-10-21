fc31f90b171236c6e9494ec42ba01bdee3a93fb7
germanUNAP <ccus.german.chura@gmail.com>
2022-10-21 15:19:45 +1300

    se añadira los archivos para que el analizador funcione

---
+++ CodigoC.cpp
@@ -0,0 +1,16 @@
+int main(){
+    float n;
+    if( n > 5 ){
+        n = 3;
+    }
+    //comentario
+    //Hola
+    int numero = 2;/*
+    holaaa
+    */
+   cout<<"Hola";
+   if(4<5){
+    cout<<"yes";
+   }    
+   cin>>numero;
+}
